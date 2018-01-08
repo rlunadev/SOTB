ae72c57de713ce66e53bb8cc607d7f87be468e40
ronaldlunaramos <ronaldwin79@hotmail.com>
2018-01-07 23:32:31 -0400

    updated configurations

---
+++ Server/Server/App_Start/PersonDBInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Server.App_Start
+{
+    public class PersonDBInitializer : DropCreateDatabaseAlways<PersonDBContext>
+    {
+        protected void Seed(PersonDBContext context)
+
+        {
+
+            var persons = new List<Person> {
+
+    new Person { Name = "Julio", LastName = "Avellaneda", Twitter =
+    "@julitogtu"},
+
+    new Person { Name = "Juan", LastName = "Ruiz", Twitter =
+    "@juankruiz"},
+
+    new Person { Name = "Roberto", LastName = "Alvarado", Twitter =
+    "@ralvaradot"},
+
+    new Person { Name = "Nicolas", LastName = "Herrera", Twitter =
+    "@nicolocodev"},
+
+    new Person { Name = "Jorge", LastName = "Ramirez", Twitter =
+    "@jramirezdev"},
+
+    new Person { Name = "Nelson", LastName = "Venegas", Twitter =
+    "@nvenegar"}
+
+    };
+
+            persons.ForEach(c => context.Person.Add(c));
+
+            context.SaveChanges();
+
+        }
+
+    }
+}
---
+++ Server/Server/Models/PersonDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Server.Models
+{
+    public class PersonDBContext : DbContext
+    {
+        public DbSet Person { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public System.Data.Entity.DbSet<Server.Models.Person> People { get; set; }
+    }
+}
